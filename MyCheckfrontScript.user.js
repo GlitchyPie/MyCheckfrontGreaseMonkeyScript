ec267041f9ae51b201d6b791b9c7eb404d2f0b30
GlitchyPie <71139548+GlitchyPie@users.noreply.github.com>
2025-06-24 14:46:32 +0100

    Update MyCheckfrontScript.user.js

--- MyCheckfrontScript.user.js
+++ MyCheckfrontScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Checkfront Overnight Report Helper Script
+// @name         Checkfront Overnight Report Helper Script DEV VERSION
 // @namespace    http://cat.checkfront.co.uk/
-// @version      2025-06-24T12:23
+// @version      DEV_2025-06-24T14:46
 // @description  Add additional reporting functions / formats to CheckFront
 // @author       GlitchyPies
 // @match        https://cat.checkfront.co.uk/*
@@ -16,7 +16,7 @@ console.log('Hello world');
 
 (function($) {
     'use strict';
-    
+
     console.log('Running main function...');
 
     //                     Column Header,  Column Value
@@ -63,7 +63,7 @@ console.log('Hello world');
     //================================== CSV Parsing =========================================
     function BADLY_PARSE_QUOTED_CSV_LINE(line){
         if(!(!!line)){return;}
-        
+
         const outputAr = [];
         let inPart = false
         let currentPart = "";
@@ -182,25 +182,21 @@ console.log('Hello world');
         return output;
     }
     //========================================================================================
-    
-    function CSV_2_HTML(data, headers){
+
+    function CSV_2_TABLE(parsed, html_headers){
         const rx = /\{(.+?)\}/ig;
         const isWhiteRx = /^\s+$/;
         const isNonGuest = /^\s*Guest\s*#\s*\d+\s*$/;
 
-        const parsed = BADLY_PARSE_CSV(data);
-        let out = '<table><thead><tr>';
-        for(var h = 0; h < headers.length; h++){
-            out += ('<th>' + headers[h][0] + '</th>');
-        }
-        out += '</tr></thead><tbody>';
+        const outputArray = [];
+        outputArray.push(parsed[0]);
 
         for(var i = 1; i < parsed.length; i++){
-            out += '<tr>';
             const line = parsed[i];
+            const output_row = [];
 
-            for(var hh = 0; hh < headers.length; hh++){
-                let headerVal = headers[hh][1];
+            for(var hh = 0; hh < html_headers.length; hh++){
+                let headerVal = html_headers[hh][1];
                 let headerValues = [];
                 let processedHeaderValues = [];
                 if(Array.isArray(headerVal)){
@@ -223,8 +219,6 @@ console.log('Hello world');
 
                     if((workingValue != '') && (!isWhiteRx.test(workingValue)) && (!isNonGuest.test(workingValue))){
                         processedHeaderValues.push([headerValues[j][0],workingValue]);
-                    }else if(headers[hh][0] == 'Room'){
-                        console.log('Uh oh');
                     }
                 }
                 if(processedHeaderValues.length === 1){
@@ -236,9 +230,54 @@ console.log('Hello world');
                     }
                 }
 
-                out += ('<td>' + headerVal + '</td>');
+                output_row.push(headerVal);
             }
+            outputArray.push(output_row);
 
+        }
+        return outputArray;
+    }
+    function TABLE_REMOVE_DUPLICATES(table){
+        const l = table.length;
+        const m = table[0].length;
+        const cleaned = [];
+        let test = false;
+        for(var i = 1; i < l; i++){
+            const a = table[i][0];
+            searchCleaned: {
+                for(var j = 0; j < cleaned.length; j++){
+                    test = false;
+                    if(cleaned[j][0] === a){
+                        test = true;
+                        for(var k = 1; k < m; k++){
+                            test = (test && (table[i][k] === cleaned[j][k]));
+                        }
+                        if(test){break searchCleaned;} //If we've found a dupicate we can exit early.
+                    }
+                }
+            }
+            if(!test){
+                cleaned.push(table[i]);
+            }
+        }
+
+        return cleaned;
+    }
+    function CSV_2_HTML(data, headers){
+        const table = TABLE_REMOVE_DUPLICATES(CSV_2_TABLE(BADLY_PARSE_CSV(data),headers));
+
+        let out = '<table><thead><tr>';
+        for(var h = 0; h < headers.length; h++){
+            out += ('<th>' + headers[h][0] + '</th>');
+        }
+        out += '</tr></thead><tbody>';
+
+        for(var i = 1; i < table.length; i++){
+            out += '<tr>';
+            const row = table[i];
+            for(const cell of row){
+                out += ('<td>' + cell + '</td>');
+            }
             out += '</tr>';
         }
         out += '</tobody></table>';
@@ -302,32 +341,55 @@ console.log('Hello world');
         });
 
 
-        const $content = $('#content');
-        const $page = $content.children().eq(0).children('div.page-lg').eq(0);
-        const $controls = $page.children().eq(0);
+        const $page = getMainPageContentAfterHeaders();
 
         $page.empty(); //This breaks stuff!
         //$page.append($controls);
+
         $page.append($table);
     }
     //========================================================================================
 
 
-    function FindPageTitle(){
-        const $content = $('#content');
-        const $pageHeaders = $content.find('div[class^="PageHeader"]');
-        const $pagelg = $pageHeaders.find('div.page-lg');
-        const $title = $pagelg.find('div[class^="Title"]').eq(0);
-
-        return $title;
+    function getContentDiv(){
+        return $('#content').children().eq(0);
     }
-    
+    function getMainPageContentAfterHeaders(){
+        const $content = getContentDiv();
+        return $content.children('div.page-lg').eq(0);
+    }
+    function findPageTitle(){
+        const $content = getContentDiv();
+        const $titleHeaders = $content.find('div[class^="PageHeader"][class*="Title"]');
+        const $pagelg = $titleHeaders.find('div.page-lg');
+        const $title = $pagelg.find('div[class^="Title"]');
+
+        return $title.eq(0);
+    }
+    function findPrimaryActionsDiv(){
+        const $content = getContentDiv();
+        const $actionBar = $content.find('div[class^="PageHeader"][class*="ActionsBar"]');
+        const $pagelg = $actionBar.find('div.page-lg');
+        const $primaryActions = $pagelg.find('div[class^="PageHeader"][class*="primaryActions"]');
+
+        return $primaryActions.eq(0);
+    }
+    function findSecondaryActionsDiv(){
+        const $content = getContentDiv();
+        const $actionBar = $content.find('div[class^="PageHeader"][class*="ActionsBar"]');
+        const $pagelg = $actionBar.find('div.page-lg');
+        const $primaryActions = $pagelg.find('div[class^="PageHeader"][class*="secondaryActions"]');
+
+        return $primaryActions.eq(0);
+    }
+
+
     function AddDailyManifestReportButtons(){
         const $newBookingButton = $('#bookingButtonDesktop');
         if($newBookingButton.length !== 1){console.log('#bookingButtonDesktop not found'); return}
 
         const $cloneButton = $newBookingButton.clone(true, false);
-        
+
         $cloneButton.attr({'id':'overnightButtonDesktop',
                            'href':'about:blank'});
         $cloneButton.children('span').text('Overnight Report');
@@ -335,6 +397,13 @@ console.log('Hello world');
         $cloneButton.on('click',(event)=>{
             event.preventDefault();
             ConvertDailyManifest_2_OvernightReport();
+            findPageTitle().text('Overnight Report');
+            const $actions = findPrimaryActionsDiv();
+            const $text = $actions.find('div[class^="Text__Text_"]').eq(0);
+            $actions.empty();
+            $actions.append($text);
+            $text.show();
+
         });
     }
     function AddReportButtons(){
@@ -347,7 +416,7 @@ console.log('Hello world');
     }
     AddReportButtons();
 
-    
+
     //=================================== Observer set up= ===================================
     function ProcessMutations(mutations, observer){
         AddReportButtons();
